ca7327320575b6a61c3085ccea24170c3c2fbef5
Mike <michael.lilley@gmail.com>
2013-06-03 21:42:13 +1000

    Added keyboard and mouse interactivity.

--- vnc-over-gif.js
+++ vnc-over-gif.js
@@ -1,22 +1,43 @@
 #!/usr/bin/env node
 
-var GifLib = require('gif');
+// NOTE: gif module currently doesn't install, so I've removed it from the package.json
+//    See workaround: https://github.com/sidorares/vnc-over-gif/issues/5
+//var GifLib = require('gif');
+var GifLib = require('/path-to-manually-compiled/node-gif/build/Release/obj.target/gif.node');
+
 var rfb = require('rfb2');
 var url = require('url');
-
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var swig = require('swig');
+var formidable = require('formidable');
+
+var r = null;
+
 http.createServer(function(req, res) {
   var params = url.parse(req.url, true);
-  if (params.pathname == '/screen.gif') {
-    var connectArgs = {
+
+  if (params.pathname == '/jquery.js') { 
+    respondWithFile(res, 'jquery.js', 'text/javascript'); 
+
+  } else if (params.pathname == '/js.html') { 
+    respondWithHtml(res, 'js.html', params); 
+
+  } else if (params.pathname == '/update' && req.method.toUpperCase() == 'POST') {
+    processUpdate(req, res, r); 
+
+  } else if (params.pathname == '/screen.gif') {
+    var args = {
       host: params.query.host || 'localhost',
       port: params.query.port ? parseInt(params.query.port,10) : 5900,
       password: params.query.password || ''
     };
-    var r = rfb.createConnection(connectArgs);
+    r = rfb.createConnection(args);
     var gif;
+  
     r.on('connect', function() {
-      res.writeHead(200, { 'Content-Type': 'image/gif'});
+      res.writeHead(200, { 'Content-Type': 'image/gif' });
       gif = new GifLib.AnimatedGif(r.width, r.height);
       gif.setOutputCallback(res.write.bind(res));
     });
@@ -46,9 +67,54 @@ http.createServer(function(req, res) {
         res.end('unknown error:' + JSON.stringify(err));
       }
     });
-
   } else {
     res.writeHead(404);
     res.end();
   }
 }).listen(process.env.PORT || 4444);
+
+var respondWithFile = function(res, filename, contentType) {
+  var stat   = fs.statSync(filename);
+  var stream = fs.createReadStream(filename);
+  res.writeHead(200, { 'Content-Type': contentType, 'Content-Length': stat.size });
+  stream.pipe(res);
+};
+
+var respondWithHtml = function(res, filename, params) {
+  var search = (params.search || '');
+  var html   = swig.compileFile(path.resolve(__dirname, filename)).render({ query: search });
+  res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': Buffer.byteLength(html, 'utf8') });
+  res.write(html);
+  res.end();
+};
+
+var processUpdate = function(req, res, r) {
+  if (r) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields) {
+      if (!err) {
+        //console.log(fields);
+        switch(fields.event) {
+        case 'mouse':
+          r.pointerEvent(fields.x, fields.y, fields.buttonState);
+          break;
+        case 'keyboard':
+          r.keyEvent(fields.keyCode, fields.isDown);
+          break;
+        case 'clipboard':
+          r.clipboardUpdate(fields.text);
+          break;
+        default: 
+          res.writeHead(400);
+          res.end("unrecognised event '"+fields.event+"'");
+          return;
+        }
+        res.writeHead(200);
+        res.end();
+      } else {
+        res.writeHead(400);
+        res.end();
+      }
+    });
+  }
+};
