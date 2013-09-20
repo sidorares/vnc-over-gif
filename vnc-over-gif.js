13737a64ff1c16578609820eaa74c7243f178f6e
Andrey Sidorov <sidorares@yandex.ru>
2013-09-20 14:29:00 +1000

    initial js-only gif encoding with omggif and NetQuant

--- vnc-over-gif.js
+++ vnc-over-gif.js
@@ -1,54 +1,27 @@
 #!/usr/bin/env node
-
-var GifLib = require('gif');
 var rfb = require('rfb2');
-var url = require('url');
+var GifStream = require('./lib/stream.js');
 
-var http = require('http');
-http.createServer(function(req, res) {
-  var params = url.parse(req.url, true);
+require('http').createServer(function(req, res) {
+  var params = require('url').parse(req.url, true);
   if (params.pathname == '/screen.gif') {
-    var connectArgs = {
-      host: params.query.host || 'localhost',
-      port: params.query.port ? parseInt(params.query.port,10) : 5900,
-      password: params.query.password || ''
-    };
-    var r = rfb.createConnection(connectArgs);
+    var r = rfb.createConnection(params.query);
     var gif;
+
     r.on('connect', function() {
       res.writeHead(200, { 'Content-Type': 'image/gif'});
-      gif = new GifLib.AnimatedGif(r.width, r.height);
-      gif.setOutputCallback(res.write.bind(res));
+      
+      gif = new GifStream(r.width, r.height);
+      //gif.pipe(res);
+      
+      r.on('rect', function(rect) {
+        var data = gif.addFrame(rect.x, rect.y, rect.width, rect.height, rect.data);
+        r.requestUpdate(true, 0, 0, r.width, r.height);
+        res.write(data);
+      });
     });
-    r.on('rect', function(rect) {
-      // todo: use BGRA buffer type in gif constructor (does not work for me for some reason)
-      var rgb = new Buffer(rect.width*rect.height*3);
-      var offset = 0;
-      for (var i=0; i < rect.buffer.length; i += 4) {
-        rgb[offset++] = rect.buffer[i+2];
-        rgb[offset++] = rect.buffer[i+1];
-        rgb[offset++] = rect.buffer[i];
-      }
-      gif.push(rgb, rect.x, rect.y, rect.width, rect.height);
-      gif.endPush();
-
-      // send on vnc disconnect?
-      // gif.end();
-    });
-    r.on('error', function(err) {
-      console.error(err);
-      res.writeHead(502, { 'Content-Type': 'text/plain'});
-      if (typeof(err) == 'string') {
-        res.end(err);
-      } else if (err instanceof Error) {
-        res.end(err.message);
-      } else {
-        res.end('unknown error:' + JSON.stringify(err));
-      }
-    });
-
   } else {
     res.writeHead(404);
     res.end();
   }
-}).listen(process.env.PORT || 4444);
+}).listen(process.env.PORT || 4444, '0.0.0.0');
